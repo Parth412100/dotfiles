6ad49d8e1ccc78282579c6674ba4c42dd92c7ad0
parth <parth412100@gmail.com>
2025-12-05 11:11:36 +0530

    updated read

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ rm ~/.local/share/applications/HEY.desktop  #removing hey
 sudo pacman -Rns --noconfirm signal-desktop obsidian 1password-beta 1password-cli
 
 echo "Installing required packages..."
-yay -S --noconfirm --needed mpv fastfetch btop git yt-dlp ani-cli blueman zen-browser-bin lvsk-calendar
+yay -S --noconfirm --needed mpv fastfetch btop git yt-dlp ani-cli blueman zen-browser-bin lvsk-calendar visual-studio-code-bin
 
 . ./calender.sh
 
